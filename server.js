021503fc3f582975740be75116a7878b1818ffb2
Wasym Shykh <creekgem@outlook.com>
2018-07-19 05:42:52 +0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(`${__dirname}/views/layout`);
+app.set('view_engine', 'hbs');
+
+hbs.registerHelper('getCurrentYear', ()=>{
+    return new Date().getFullYear();
+})
+
+hbs.registerHelper('screamIt', (data)=>{
+    return data.toUpperCase();
+})
+
+app.use((request, response, next)=>{
+
+    let log = `${new Date().toString()}: ${request.method} ${request.url}`;
+
+    console.log(log);
+    fs.appendFile('logger.txt', log, (err)=>{
+        if(err){
+            console.log('Error with fs');
+        }
+    });
+    next();
+});
+
+// app.use((require, response, next)=>{
+//     response.render('maintenance.hbs');
+// });
+
+app.use(express.static(`${__dirname}/public`));
+
+
+app.get('/', (request, response)=>{
+
+    response.render('home.hbs', {
+        pageTitle: 'Home Page',
+        pageMessage: 'This is a home page of the site',
+    });
+
+});
+
+app.get('/about', (request, response)=>{
+
+    response.render('about.hbs', {
+        pageTitle: 'About US',
+    });
+
+});
+
+app.get('/bad', (request, response)=>{
+    
+    response.send({
+        errorMessage: 'Failed to retrive the page'
+    })
+
+})
+
+app.listen(3000, ()=>{
+    console.log('Server is up & running on port 3000');
+    
+});
